8f74faa64028c3de658bc0505b21c82977e28d57
Kurt Jacobson <kcjengr@gmail.com>
2020-09-26 15:40:40 -0400

    include linuxcnc files in wheel

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os, sys
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
@@ -5,6 +6,35 @@ with open("README.md", "r") as fh:
 
 with open("entry_points.ini", "r") as fh:
     entry_points = fh.read()
+
+
+# list of (destination, source_file) tuples
+DATA_FILES = [
+    # ('~/', ['scripts/.xsessionrc',]),
+]
+
+# list of (destination, source_dir) tuples
+DATA_DIRS = [
+    ('share/linuxcnc/configs/sim.plasma-flat/', 'linuxcnc/configs/sim.plasma-flat'),
+    ('share/linuxcnc/nc_files/plasma-flat', 'linuxcnc/nc_files/plasma-flat'),
+]
+
+
+def data_files_from_dirs(data_dirs):
+    data_files = []
+    for dest_dir, source_dir in data_dirs:
+        dest_dir = os.path.expandvars(dest_dir)
+        for root, dirs, files in os.walk(source_dir):
+            root_files = [os.path.join(root, i) for i in files]
+            dest = os.path.join(dest_dir, os.path.relpath(root, source_dir))
+            data_files.append((dest, root_files))
+
+    return data_files
+
+
+data_files = [(os.path.expanduser(dest), src_list) for dest, src_list in DATA_FILES]
+data_files.extend(data_files_from_dirs(DATA_DIRS))
+
 
 setup(
     name="plasma-flat",
@@ -17,6 +47,7 @@ setup(
     url="https://github.com/kurtjacobson/plasma-flat",
     download_url="https://github.com/kurtjacobson/plsama-flat/tarball/master",
     packages=find_packages(),
+    data_files=data_files,
     include_package_data=True,
     entry_points=entry_points,
     # install_requires=[
