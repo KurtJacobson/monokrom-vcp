a8f34a5cf31d4101291b774acb88fdff6659d1d9
Kurt Jacobson <kcjengr@gmail.com>
2020-10-03 14:55:11 -0400

    top to patch QTabWidget again

--- monokrom/common/__init__.py
+++ monokrom/common/__init__.py
@@ -9,9 +9,10 @@ def QGroupBox_resizeEvent(self, event):
 QGroupBox.resizeEvent = QGroupBox_resizeEvent
 
 # patch QTabWidget so tabs expand to full width
+# FixMe: this is only needed on older versions of Qt,
+# so we should only patch if needed.
 def QTabWidget_resizeEvent(self, event):
     super(QTabWidget, self).resizeEvent(event)
-    qss = "::tab-bar {min-width: %ipx;}" % self.width()
-    self.setStyleSheet(qss)
+    self.tabBar().setFixedWidth(self.width())
 
 QTabWidget.resizeEvent = QTabWidget_resizeEvent
