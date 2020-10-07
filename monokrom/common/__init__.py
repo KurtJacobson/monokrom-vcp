5c7b2cb4cefe2c543df518dbdea4f6777591c6e4
Kurt Jacobson <kcjengr@gmail.com>
2020-10-06 21:09:59 -0400

    best not to patch QGroupBox

--- monokrom/common/__init__.py
+++
@@ -1,9 +0,0 @@
-from qtpy.QtWidgets import QGroupBox, QTabWidget, QWidget
-
-# patch QGroupBox so title expands to full width
-def QGroupBox_resizeEvent(self, event):
-    super(QGroupBox, self).resizeEvent(event)
-    qss = "::title {min-width: %ipx;}" % self.width()
-    self.setStyleSheet(qss)
-
-QGroupBox.resizeEvent = QGroupBox_resizeEvent
--- monokrom/common/widgets/group_box.py
+++ monokrom/common/widgets/group_box.py
@@ -6,7 +6,7 @@ class MkGroupBox(QGroupBox):
     def __init__(self, parent=None):
         super(MkGroupBox, self).__init__(parent)
 
-    # def resizeEvent(self, event):
-    #     super(MkGroupBox, self).resizeEvent(event)
-    #     qss = "::title {min-width: %ipx;}" % self.width()
-    #     self.setStyleSheet(qss)
+    def resizeEvent(self, event):
+        super(MkGroupBox, self).resizeEvent(event)
+        qss = "::title {min-width: %ipx;}" % self.width()
+        self.setStyleSheet(qss)
