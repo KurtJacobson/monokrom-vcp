c53179307477d415a13c35be362dc72ca01808e0
Kurt Jacobson <kcjengr@gmail.com>
2021-01-01 13:16:26 -0500

    change display to match state

--- monokrom/mill/mainwindow.py
+++ monokrom/mill/mainwindow.py
@@ -18,12 +18,13 @@ class MainWindow(VCPMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
+        # connect widget signals
         self.mainModeTabWidget.currentChanged.connect(self.onModeTabIndexChanged)
 
+        # connect status signals
         STATUS.task_mode.notify(self.onTaskModeChanged)
         STATUS.task_state.onValueChanged(self.onTaskStateChanged)
         STATUS.interp_state.notify(self.onInterpStateChanged)
-
 
     def onTaskStateChanged(self, task_state):
         self.mainModeTabWidget.setEnabled(task_state == linuxcnc.STATE_ON)
@@ -36,19 +37,20 @@ class MainWindow(VCPMainWindow):
         else:
             setTaskMode(linuxcnc.MODE_AUTO)
 
-    # add any custom methods here
     def onTaskModeChanged(self, task_mode):
         if task_mode == linuxcnc.MODE_MANUAL:
             self.mainModeTabWidget.setCurrentIndex(0)
         elif task_mode == linuxcnc.MODE_MDI:
             self.mainModeTabWidget.setCurrentIndex(1)
+            self.programStackedWidget.setCurrentIndex(1)
         elif task_mode == linuxcnc.MODE_AUTO:
             self.mainModeTabWidget.setCurrentIndex(2)
+            self.programStackedWidget.setCurrentIndex(0)
 
     def onInterpStateChanged(self, interp_state):
-        if interp_state == linuxcnc.INTERP_IDLE:
-            self.mainModeTabWidget.setTabEnabled(0, True)
-            self.mainModeTabWidget.setTabEnabled(1, True)
-        else:
-            self.mainModeTabWidget.setTabEnabled(0, False)
-            self.mainModeTabWidget.setTabEnabled(1, False)
+            if interp_state == linuxcnc.INTERP_IDLE:
+                self.mainModeTabWidget.setTabEnabled(0, True)
+                self.mainModeTabWidget.setTabEnabled(1, True)
+            else:
+                self.mainModeTabWidget.setTabEnabled(0, False)
+                self.mainModeTabWidget.setTabEnabled(1, False)
